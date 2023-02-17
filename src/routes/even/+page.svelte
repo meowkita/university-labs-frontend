9546bb6bd704ccc669439d334cf791f05d16b6ec
meowkita <nikita.lossinov@gmail.com>
2023-02-17 20:31:40 +0600

    Initial commit

---
+++ src/routes/even/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { postData } from '../../js/fetchWrapper'
+
+  let number = 0
+  let isEven
+
+  function submit() {
+    postData('http://localhost:8080/api/even?number=' + number).then((data) => {
+      isEven = data
+    })
+  }
+</script>
+
+<div class="container">
+  <h1>Лабораторная работа №2</h1>
+  <h3>Проверка числа на четность/нечетность</h3>
+  <div>
+    <table>
+      <tr>
+        <td>Число:</td>
+        <td><input type="text" bind:value={number} /></td>
+      </tr>
+    </table>
+  </div>
+  <div />
+  <div>
+    <button on:click={submit}>Проверить</button>
+    {#if isEven !== undefined}
+      <p>Число <span class="color-accent">{isEven ? 'чётное' : 'нечётное'}</span></p>
+    {:else}
+      <p>Введите данные и нажмите кнопку "Рассчитать"</p>
+    {/if}
+  </div>
+</div>
---
+++ src/routes/runner/+page.svelte
@@ -0,0 +1,50 @@
+<script>
+  import { postData } from '../../js/fetchWrapper'
+
+  let distance = 1000
+  let minutes = 1
+  let seconds = 0
+  let speed = 0
+
+  function submit() {
+    postData('http://localhost:8080/api/runner', { distance, time: minutes + '.' + seconds }).then(
+      (data) => {
+        speed = data
+      }
+    )
+  }
+</script>
+
+<div class="container">
+  <h1>Лабораторная работа №1-2</h1>
+  <h3>Вычисление скорости бега</h3>
+  <div>
+    <table>
+      <tr>
+        <td>Длина дистанции:</td>
+        <td><input type="text" bind:value={distance} /></td>
+      </tr>
+      <tr>
+        <td>
+          Время забега:
+          <br />
+          (минуты.секунды)
+        </td>
+        <td>
+          <input type="text" bind:value={minutes} />
+          <br />
+          <input type="text" class="w-40" bind:value={seconds} />
+        </td>
+      </tr>
+    </table>
+  </div>
+  <div />
+  <div>
+    <button on:click={submit}>Рассчитать</button>
+    {#if speed > 0}
+      <p>Скорость: <span class="color-accent">{speed}</span> км/час</p>
+    {:else}
+      <p>Введите данные и нажмите кнопку "Рассчитать"</p>
+    {/if}
+  </div>
+</div>
